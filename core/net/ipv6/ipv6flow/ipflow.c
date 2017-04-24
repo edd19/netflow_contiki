11cc5da63203cea95dd50911a5b40884291af79e
Ndizera Eddy <eddyndizera@gmail.com>
2017-04-24 10:49:31 +0200

    send periodically ipfix messages

--- core/net/ipv6/ipv6flow/ipflow.c
+++ core/net/ipv6/ipv6flow/ipflow.c
@@ -29,10 +29,16 @@
 
 
 static int status = 0;
+static ipfix_t *ipflow_ipfix = NULL;
+
+static struct uip_udp_conn *exporter_connection;
+static uip_ipaddr_t collector_addr;
 /*---------------------------------------------------------------------------*/
 static void initialize();
 static int cmp_ipaddr(uip_ipaddr_t *in, uip_ipaddr_t *out);
 static flow_t * create_flow(uip_ipaddr_t *destination, uint16_t size, uint16_t packets);
+static ipfix_t * ipfix_for_ipflow();
+static void send_ipfix_message(int type);
 /*---------------------------------------------------------------------------*/
 void
 launch_ipflow()
@@ -54,6 +60,10 @@ initialize()
 
   ipflow_p = PROCESS_CURRENT();
   netflow_event = process_alloc_event();
+
+  uip_ip6addr(&collector_addr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
+
+  ipflow_ipfix = ipfix_for_ipflow();
 }
 /*---------------------------------------------------------------------------*/
 static int
@@ -144,6 +154,57 @@ flush_flow_table()
   }
 }
 /*---------------------------------------------------------------------------*/
+uint8_t *
+get_octet_delta_count()
+{
+  flow_t *flow = list_head(LIST_FLOWS_NAME);
+  return (uint8_t *)&(flow -> size) ;
+}
+/*---------------------------------------------------------------------------*/
+uint8_t *
+get_packet_delta_count()
+{
+  flow_t *flow = list_head(LIST_FLOWS_NAME);
+  return (uint8_t *)&(flow -> packets);
+}
+/*---------------------------------------------------------------------------*/
+// uint8_t *
+// get_source_address()
+// {
+//   // TODO get host address
+// }
+/*---------------------------------------------------------------------------*/
+uint8_t *
+get_destination_address()
+{
+  flow_t *flow =list_pop(LIST_FLOWS_NAME);
+  return (uint8_t *)&(flow -> destination);
+}
+/*---------------------------------------------------------------------------*/
+static ipfix_t *
+ipfix_for_ipflow()
+{
+  template_t *template = create_ipfix_template(256, &get_number_flows);
+
+  add_element_to_template(template, OCTET_DELTA_COUNT);
+  add_element_to_template(template, PACKET_DELTA_COUNT);
+  add_element_to_template(template, DESTINATION_IPV6_ADDRESS);
+
+  ipfix_t *ipfix = create_ipfix();
+  add_templates_to_ipfix(ipfix, template);
+
+  return ipfix;
+}
+/*---------------------------------------------------------------------------*/
+static void
+send_ipfix_message(int type)
+{
+  uint8_t message[200];
+  int length = generate_ipfix_message(message, ipflow_ipfix, type)
+
+  uip_udp_packet_sendto(exporter_connection, &message, length * sizeof(uint8_t), &collector_addr, UIP_HTONS(COLLECTOR_UDP_PORT));
+}
+/*---------------------------------------------------------------------------*/
 PROCESS(flow_process, "Ip flows");
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(flow_process, ev, data)
@@ -154,11 +215,21 @@ PROCESS_THREAD(flow_process, ev, data)
 
   initialize();
 
+  exporter_connection = udp_new(NULL, UIP_HTONS(COLLECTOR_UDP_PORT), NULL); 
+  if(exporter_connection == NULL) {
+    PRINTF("No UDP connection available, exiting the process!\n");
+    PROCESS_EXIT();
+  }
+  udp_bind(exporter_connection, UIP_HTONS(COLLECTOR_UDP_PORT));
+
+  send_ipfix_message(IPFIX_TEMPLATE)
+
   etimer_set(&periodic, IPFLOW_EXPORT_INTERVAL*60*CLOCK_SECOND);
   while(1){
     PROCESS_YIELD();
     if(etimer_expired(&periodic)) {
       etimer_reset(&periodic);
+      send_ipfix_message(IPFIX_DATA);
       flush_flow_table();
     }
   }
--- core/net/ipv6/ipv6flow/ipflow.h
+++ core/net/ipv6/ipv6flow/ipflow.h
@@ -11,11 +11,11 @@
 #define IPFLOW_H_
 /*---------------------------------------------------------------------------*/
 #include "net/ip/uip.h"
+#include "net/ipv6/tinyipfix/tipfix.h"
 /*---------------------------------------------------------------------------*/
 #define MAX_FLOWS 10
-
 #define IPFLOW_EXPORT_INTERVAL 1 // minute
-
+#define COLLECTOR_UDP_PORT 9995
 /*---------------------------------------------------------------------------*/
 
 /** Structures definition **/
@@ -33,5 +33,18 @@ int get_process_status();
 int update_flow_table(uip_ipaddr_t *destination, uint16_t size, uint16_t packets);
 int get_number_flows();
 void flush_flow_table();
+
+uint8_t * get_octet_delta_count();
+uint8_t * get_packet_delta_count();
+// uint8_t * get_source_address();
+uint8_t * get_destination_address();
+
+/*---------------------------------------------------------------------------*/
+
+/** INFORMATION ELEMENTS FIELDS **/
+#define OCTET_DELTA_COUNT create_ipfix_information_element(1, 2, 0, &get_octet_delta_count)
+#define PACKET_DELTA_COUNT create_ipfix_information_element(2, 2, 0, &get_packet_delta_count)
+// #define SOURCE_IPV6_ADDRESS create_ipfix_information_element(27, 16, 0, &get_source_address);
+#define DESTINATION_IPV6_ADDRESS create_ipfix_information_element(28, 16, 0, &get_destination_address);
 
 #endif /* IPFLOW_H_ */
